0008653bfc9284f3a5fa755a4ee5aa6be2f81980
Fenshan <claranicolasviolant@gmail.com>
2020-02-01 12:56:30 +0200

    push try

---
+++ Repair/Assets/Scripts/Scroller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroller : MonoBehaviour
+{
+    int CAMERA_MIN = 5;
+    int CAMERA_MAX = 25;
+    float ratio = 15;
+
+    private void Start()
+    {
+        Camera.main.orthographicSize = 5;
+    }
+
+    void Update()
+    {
+        AutomaticScroller();
+        float cameraAux = Camera.main.orthographicSize - Input.mouseScrollDelta.y * Time.deltaTime * ratio;
+        if (cameraAux >= CAMERA_MIN && cameraAux <= CAMERA_MAX) Camera.main.orthographicSize = cameraAux;
+
+
+
+
+
+
+
+
+
+    }
+
+    void AutomaticScroller()
+    {
+
+    }
+
+    /*
+     * Input.GetAxis("Mouse ScrollWheel")
+     * Input.mouseScrollDelta.y
+     */
+}
