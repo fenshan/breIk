da96bc646270fa64f3e0d01f95e378958e9fe8ab
Fenshan <claranicolasviolant@gmail.com>
2020-02-02 16:54:41 +0200

    deactivate scroller

--- Repair/Assets/Scripts/CameraScroller.cs
+++ Repair/Assets/Scripts/CameraScroller.cs
@@ -9,8 +9,8 @@ public class CameraScroller : MonoBehaviour
     public Vector3 CAMERA_MIN; //just the screen of the laptop. 0 scroll
     public Vector3 CAMERA_MAX; //whole room. 1 scroll
     public float speedManualScroll;
-    public float speedAutomaticScroll;
-    int chanceToAutomaticScroll;
+    //public float speedAutomaticScroll;
+    //int chanceToAutomaticScroll;
 
     AudioSource goodTheme, badTheme;
     public AudioClip good, bad;
@@ -27,14 +27,15 @@ public class CameraScroller : MonoBehaviour
         badTheme.Play();
         //
         currentScroll = 0;
-        chanceToAutomaticScroll = 500;
+        //chanceToAutomaticScroll = 500;
     }
 
     void Update()
     {
         //UPDATE SCROLL
-        float cameraScrollAux = currentScroll + Time.deltaTime * AutomaticScroller();
-        if (Input.mouseScrollDelta.y > 0) cameraScrollAux -= Input.mouseScrollDelta.y * Time.deltaTime * speedManualScroll; //Manual Scroll is only available to Scroll In
+        //float cameraScrollAux = currentScroll + Time.deltaTime * AutomaticScroller();
+        //if (Input.mouseScrollDelta.y > 0) cameraScrollAux -= Input.mouseScrollDelta.y * Time.deltaTime * speedManualScroll; //Manual Scroll is only available to Scroll In
+        float cameraScrollAux = currentScroll  - Input.mouseScrollDelta.y * Time.deltaTime * speedManualScroll;
         if (cameraScrollAux > 1) cameraScrollAux = 1;
         else if (cameraScrollAux < 0) cameraScrollAux = 0;
         Camera.main.transform.position = Vector3.Lerp(CAMERA_MIN, CAMERA_MAX, cameraScrollAux);
@@ -42,23 +43,23 @@ public class CameraScroller : MonoBehaviour
         SetMusic();
     }
 
-    //Automatic Scroll out (depending on CurrentAnxietyLevel)
-    float AutomaticScroller()
-    {
-        //INCREASE AUTOMATICSPEED [logarithmic function: 1.649*log(0.263*x)*manualScroll]
-        speedAutomaticScroll = 1.649f * Mathf.Log(0.263f * CurrentAnxietyLevel) * speedManualScroll;
+    ////Automatic Scroll out (depending on CurrentAnxietyLevel)
+    //float AutomaticScroller()
+    //{
+    //    //INCREASE AUTOMATICSPEED [logarithmic function: 1.649*log(0.263*x)*manualScroll]
+    //    speedAutomaticScroll = 1.649f * Mathf.Log(0.263f * CurrentAnxietyLevel) * speedManualScroll;
 
-        if (CurrentAnxietyLevel < 5) return 0;
-        //CHANCE TO AUTOMATIC SCROLL
-        if (Random.Range(0, chanceToAutomaticScroll) == 0)
-        {
-            //INCREASE CHANCE
-            if (chanceToAutomaticScroll > 10) chanceToAutomaticScroll -= 40;
-            else chanceToAutomaticScroll = 10;
-            return speedManualScroll;
-        }
-        else return 0;
-    }
+    //    if (CurrentAnxietyLevel < 5) return 0;
+    //    //CHANCE TO AUTOMATIC SCROLL
+    //    if (Random.Range(0, chanceToAutomaticScroll) == 0)
+    //    {
+    //        //INCREASE CHANCE
+    //        if (chanceToAutomaticScroll > 10) chanceToAutomaticScroll -= 40;
+    //        else chanceToAutomaticScroll = 10;
+    //        return speedManualScroll;
+    //    }
+    //    else return 0;
+    //}
 
     void SetMusic()
     {
